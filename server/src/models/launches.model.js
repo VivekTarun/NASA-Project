dcac8f2ee3b81fb90a43294e1bd08519ce6ed61c
vivektarun <vivektarun1234@gmail.com>
2023-08-12 19:19:25 +0530

    posting done | validation of data done

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,13 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
 
 const launch = {
     flightNumber: 100,
-    mission: 'kepler Exploration X', 
+    mission: 'kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('Devember 27, 2030'),
-    destination: 'kepler-442 b', 
-    customer: ['ZTM', 'NASA'],
+    destination: 'kepler-442 b',
+    customer: ['VIVEK', 'NASA'],
     upcoming: true,
     success: true,
 
@@ -19,6 +20,20 @@ function getAllLaunches() {
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            success: true,
+            upcoming : true,
+            customers: ['vivek tarun', 'NASA'],
+            flightNumber: latestFlightNumber,
+        })
+    );
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
